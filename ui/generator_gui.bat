7a4e9d8136e6e30d18730c8a7f267d69c858f159
David Sasso <davidsasso.15@gmail.com>
2023-11-04 14:44:35 -0600

    - Using logos on GUI

--- ui/generator_gui.bat
+++ ui/generator_gui.bat
@@ -1,2 +1,2 @@
 @echo off
-cmd /k "cd /d C:\ITW\itw-tester\virtualenv\Scripts & activate & cd C:\ITW\itw-tester\ui & pyuic5 gui.ui -o gui.py & exit"
+cmd /k "cd /d C:\ITW\itw-tester\virtualenv\Scripts & activate & cd C:\ITW\itw-tester\ui & pyuic5 gui.ui -o gui.py & cd C:\ITW\itw-tester\resources & pyrcc5 resources.qrc -o resources.py & exit"
